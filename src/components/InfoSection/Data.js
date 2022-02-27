eaf6e3e28e158d6df054b2aac7a072d1f0dcf2da
Jtuppack <jtuppack@gmail.com>
2022-02-27 11:54:27 +1000

    Added sections dynamically using data file

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+//Add different styling based on the sections
+//Easy way to adjust values for the landing page
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transtions without getting any fees.',
+    buttonLabel: 'Get started',
+    imgStart: true,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transtions without getting any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: false,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transtions without getting any fees.',
+    buttonLabel: 'Get started',
+    imgStart: true,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection'
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
 
 const Home = () => {
 
@@ -17,6 +19,9 @@ const Home = () => {
     <Sidebar isOpen={isOpen} toggle={toggle} />
     <Navbar toggle={toggle} />
     <HeroSection />
+    <InfoSection {...homeObjOne}/>
+    <InfoSection {...homeObjTwo}/>
+    <InfoSection {...homeObjThree}/>
     </>
   )
 }
