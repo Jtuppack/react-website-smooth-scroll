5ce730e283bfa695a57da17cbd01e847ff5dda04
Jtuppack <jtuppack@gmail.com>
2022-02-27 13:31:29 +1000

    Added the footer to the Document

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -28,7 +28,7 @@ export const homeObjTwo = {
     description: 'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
     buttonLabel: 'Learn More',
     imgStart: false,
-    img: require('../../images/svg-2.svg'),
+    img: require('../../images/svg-1.svg'),
     alt: 'Piggybank',
     dark: false,
     primary: false,
@@ -37,34 +37,18 @@ export const homeObjTwo = {
 
 export const homeObjThree = {
     id: 'signup',
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
     topLine: '',
     headline: 'Join our Team',
     description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
     buttonLabel: 'Start Now',
-    imgStart: true,
+    imgStart: false,
     img: require('../../images/svg-3.svg'),
     alt: 'Paper',
-    dark: true,
-    primary: true,
-    darkText: false
-}
-
-export const homeObjFour = {
-    id: 'discover',
-    lightBg: true,
-    lightText: false,
-    lightTextDesc: false,
-    topLine: 'Unlimited Access',
-    headline: 'Log in to your account at any time',
-    description: 'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
-    buttonLabel: 'Learn More',
-    imgStart: false,
-    img: require('../../images/svg-2.svg'),
-    alt: 'Piggybank',
     dark: false,
     primary: false,
     darkText: true
 }
+
